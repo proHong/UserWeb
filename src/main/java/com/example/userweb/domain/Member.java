c2e922b7355f2cdd32cb37a2c2e8d65d9ea6196d
Hong <tjdgus0454@gmail.com>
2020-06-09 14:37:17 +0900

    updateMemberApiModel

--- src/main/java/com/example/userweb/domain/Member.java
+++ src/main/java/com/example/userweb/domain/Member.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Getter
@@ -17,17 +18,22 @@ import lombok.*;
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(value = "멤버 넘버", hidden = true)
     private Long m_no;
 
     @Column(nullable = false, unique = true, length = 30)
+    @ApiModelProperty(value = "멤버 아이디", required = true)
     private String id;
 
     @Column(nullable = false, length = 100)
+    @ApiModelProperty(value = "멤버 비밀번호", required = true)
     private String pw;
 
     @Column(nullable = false, length = 100)
+    @ApiModelProperty(value = "멤버 이름", required = true)
     private String name;
 
     @CreationTimestamp
+    @ApiModelProperty(value = "등록일자", required = false)
     private Timestamp regdate;
 }
